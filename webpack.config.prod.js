d6923173e0f0231057e121c0fef68090404c304e
Scott Long <longstoryscott@gmail.com>
2019-07-31 14:59:12 -0500

    fix build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -25,21 +25,5 @@ module.exports = {
         }
       }
     ]
-  },
-  externals: [
-    {
-      'react': {
-        root: 'React',
-        commonjs2: 'react',
-        commonjs: 'react',
-        amd: 'react'
-      },
-      'react-dom': {
-        root: 'ReactDom',
-        commonjs2: 'react-dom',
-        commonjs: 'react-dom',
-        amd: 'react-dom'
-      }
-    }
-  ],
+  }
 };

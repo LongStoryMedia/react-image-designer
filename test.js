04d0821fc428e84ee3815826b833ef33f05cb129
Scott Long <longstoryscott@gmail.com>
2019-08-05 23:06:45 -0500

    stricter rules for loading to avoid many simultaneous images loading

--- test.js
+++ test.js
@@ -13,11 +13,6 @@ process.on('unhandledRejection', err => {
 const jest = require('jest');
 const argv = process.argv.slice(2);
 argv.push('--env=jsdom');
-// Watch unless on CI or in coverage mode
-if (!process.env.CI && argv.indexOf('--coverage') < 0) {
-  console.log('watch mode...')
-  argv.push('--watchAll');
-}
 
 
 jest.run(argv);

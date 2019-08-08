75fdcfcf87806b4793caf119c467133f3bfc0644
Scott Long <longstoryscott@gmail.com>
2019-08-08 10:22:58 -0500

    README, LICENSE, config

--- umd.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import ImageDesigner from "./";
-
-({ src, placeholder, domId, ...props }) =>
-  render(
-    <ImageDesigner src={src} placeholder={placeholder} {...props} />,
-    document.getElementById(domId)
-  );
